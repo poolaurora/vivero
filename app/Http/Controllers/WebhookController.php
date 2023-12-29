8aa5ffc405f8079fa4a21e898d73eff06d3e78d4
Heitor Bc <heitorbc@MacBook-Air-de-Heitor.local>
2023-12-29 10:08:13 -0300

    Improve options

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -12,26 +12,32 @@ class WebhookController extends Controller
     public function purchaseWebhook(Request $request){
 
         $data = $request->all();
-
+    
         // Acessar a matriz de comissões
         $commissions = $data['commission'];
-
-        // Verificar se a matriz de comissões não está vazia
-        $firstCommission = $commissions[1];
-        $amount = $firstCommission['commission_amount'];
-        $valorReal = number_format($amount, 2, ',', '.');
-
-
-        // Webhook Information
-        $dataEnvio = now()->format('d/m/Y H:i:s');
-        $mensagem = "**Venda realizada**\nSua comissão ⪼ R$  {$valorReal}";
-        $webhookUrl = 'https://discord.com/api/webhooks/1189573132902748212/66VYdjjfTBRna_cJxTIGHXph4ex8FUf26eiAvTJPR0oBY6aDLrS6R9dij_yEwcvgWFD7';
-        $response = Http::post($webhookUrl, ['content' => $mensagem]);
-
-
-
+    
+        // Encontrar a comissão com o nome "PerfectPay"
+        $perfectPayCommission = null;
+        foreach ($commissions as $commission) {
+            if (isset($commission['email']) && $commission['email'] === 'cryptominersbrasil@outlook.com') {
+                $perfectPayCommission = $commission;
+                break;
+            }
+        }
+    
+        if ($perfectPayCommission) {
+            // Comissão "PerfectPay" encontrada, você pode acessar os dados relevantes aqui
+            $commissionAmount = $perfectPayCommission['commission_amount'];
+            $formattedAmount = number_format($commissionAmount, 2, ',', '.');
+    
+            // Webhook Information
+            $dataEnvio = now()->format('d/m/Y H:i:s');
+            $mensagem = "**Venda realizada**\nComissão ⪼ R$  {$formattedAmount}";
+            $webhookUrl = 'https://discord.com/api/webhooks/1189573132902748212/66VYdjjfTBRna_cJxTIGHXph4ex8FUf26eiAvTJPR0oBY6aDLrS6R9dij_yEwcvgWFD7';
+            $response = Http::post($webhookUrl, ['content' => $mensagem]);
+        }
+    
         return response()->json(200);
-
-    }
+    }    
 
 }
