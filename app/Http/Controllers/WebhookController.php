0235e1377f406b46048085ef691d11b225b3cbc6
dnkzineo <pamelatomazbotmines@gmail.com>
2024-06-26 19:35:08 -0300

    first commit

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -9,35 +9,34 @@ use Illuminate\Http\Request;
 class WebhookController extends Controller
 {
     
-    public function purchaseWebhook(Request $request){
+    public function purchaseWebhook(Request $request)
+    {
+        $data = $request->all();
 
+        // URL para a qual você deseja enviar o corpo do POST
+        $url = 'https://auroraminers.com/api/process/webhook/order';
+
+        // Envie a solicitação POST
+        $response = Http::post($url, $data);
+
+        // Opcional: você pode verificar a resposta e fazer algo com ela
+        if ($response->successful()) {
+            // A solicitação foi bem-sucedida
+        } else {
+            // A solicitação falhou
+        }
+
+        return response()->json(200);
+    }
+
+    public function RefundWebhook(Request $request) {
         $data = $request->all();
     
-        // Acessar a matriz de comissões
-        $commissions = $data['commission'];
-    
-        // Encontrar a comissão com o nome "PerfectPay"
-        $perfectPayCommission = null;
-        foreach ($commissions as $commission) {
-            if (isset($commission['email']) && $commission['email'] === 'cryptominersbrasil@outlook.com') {
-                $perfectPayCommission = $commission;
-                break;
-            }
-        }
-    
-        if ($perfectPayCommission) {
-            // Comissão "PerfectPay" encontrada, você pode acessar os dados relevantes aqui
-            $commissionAmount = $perfectPayCommission['commission_amount'];
-            $formattedAmount = number_format($commissionAmount, 2, ',', '.');
-    
-            // Webhook Information
-            $dataEnvio = now()->format('d/m/Y H:i:s');
-            $mensagem = "**Venda realizada**\nComissão ⪼ R$  {$formattedAmount}";
-            $webhookUrl = 'https://discord.com/api/webhooks/1189573132902748212/66VYdjjfTBRna_cJxTIGHXph4ex8FUf26eiAvTJPR0oBY6aDLrS6R9dij_yEwcvgWFD7';
-            $response = Http::post($webhookUrl, ['content' => $mensagem]);
-        }
-    
-        return response()->json(200);
-    }    
+        // Webhook Information
+        $dataEnvio = now()->format('d/m/Y H:i:s');
+        $mensagem = "**REEMBOLSO REALIZADO**\nCompra id:{$idCompra}";
+        $webhookUrl = 'https://discord.com/api/webhooks/1197234759022162012/daMBYVEmAvIr0N21U3FT7oB3DhSkDY7H_t8t7dL5demLrwjqXNxRWYMmWQYiA4Pyumyi';
+        $response = Http::post($webhookUrl, ['content' => $mensagem]);
+    }
 
 }
